6a7bec4314e24b5257b9ecac5b1876ca1a2e5259
Dawid <215920@edu.p.lodz.pl>
2020-09-15 21:22:58 +0200

    generic paging controlller service added

--- src/main/java/pl/polishstation/polishstationbackend/apiutils/paged/PagedDomainController.java
+++ src/main/java/pl/polishstation/polishstationbackend/apiutils/paged/PagedDomainController.java
@@ -1,4 +1,4 @@
-package pl.polishstation.polishstationbackend.apiutils;
+package pl.polishstation.polishstationbackend.apiutils.paged;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
--- src/main/java/pl/polishstation/polishstationbackend/apiutils/paged/PagedDomainService.java
+++ src/main/java/pl/polishstation/polishstationbackend/apiutils/paged/PagedDomainService.java
@@ -1,4 +1,4 @@
-package pl.polishstation.polishstationbackend.apiutils;
+package pl.polishstation.polishstationbackend.apiutils.paged;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
